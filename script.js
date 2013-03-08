ecbc3387f13ff715161800d61925e96969268e63
Tianyu <noterrain@gmail.com>
2013-03-08 00:08:55 -0500

    app(1)
    Transition of videos

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    console.log("Hi");
+    //var $item = $('.reply, .submit');
+    $('#enter').click(function(){
+        //$item.fadeOut(1000);
+        console.log("Hello");
+        $("#video_background").hide();
+        $("#video2").show();
+    });
+});
+
